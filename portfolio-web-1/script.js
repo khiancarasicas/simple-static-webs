cf87176674fd98ae2eb391849ac8681ea8a3cbd2
khiancarasicas <khianreendy.c@gmail.com>
2024-12-09 18:07:13 +0800

    Final Fix

--- portfolio-web-1/script.js
+++ portfolio-web-1/script.js
@@ -11,23 +11,24 @@ document.addEventListener("DOMContentLoaded", () => {
 
         // Info.
         document.getElementById("hero-name").textContent = info.name;
-            document.getElementById("hero-title").textContent = info.title;
-            const mailLink = document.getElementById("hero-mail");
-            mailLink.textContent = info.mail;
-            mailLink.setAttribute("href", `mailto:${info.mail}`);
-            document.getElementById("about-text").textContent = info.about;
-            document.getElementById("profile-pic").src = info["profile-pic"];
-            document.getElementById("footer-text").textContent = `${info.name} - ${info.title}`;
+        document.getElementById("hero-title").textContent = info.title;
+        const mailLink = document.getElementById("hero-mail");
+        mailLink.textContent = info.mail;
+        mailLink.setAttribute("href", `mailto:${info.mail}`);
+        document.getElementById("about-text").textContent = info.about;
+        document.getElementById("profile-pic").src = info["profile-pic"];
+        document.getElementById("footer-text").textContent = `${info.name} - ${info.title}`;
+        const socialLinksContainer = document.getElementById("social-links-list");
+        info.social.forEach(link => {
+            const anchor = document.createElement("a");
+            anchor.classList.add("btn-link")
+            anchor.href = link.link;
+            anchor.target = "_blank";
+            anchor.textContent = link.name;
+            socialLinksContainer.appendChild(anchor);
+        });
 
-            const socialLinksContainer = document.getElementById("social-links-list");
-            info.social.forEach(link => {
-                const anchor = document.createElement("a");
-                anchor.classList.add("btn-link")
-                anchor.href = link.link;
-                anchor.target = "_blank";
-                anchor.textContent = link.name;
-                socialLinksContainer.appendChild(anchor);
-            });
+        document.title = `Portfolio - ${info.name}`;
 
         // Experience
         const experiencesList = document.getElementById("experience-list");
